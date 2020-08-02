498303c49035aba1f41c5ad976f5423853b65594
JeongJin984 <57170922+JeongJin984@users.noreply.github.com>
2020-08-02 15:13:05 +0900

    back and front cors problem [hotfix]

--- back/app.js
+++ back/app.js
@@ -32,7 +32,7 @@ if(prod) {
 	app.use(hpp())
 	app.use(helmet())
 	app.use(cors({
-		origin: [ 'http://localhost:3000', 'webworks.kr' ],
+		origin: [ 'http://localhost:3000', 'webworks.kr', "http://http://15.164.233.1/" ],
 		credentials: true
 	}))
 } else {
