85ab49a53030b0eaa716b0c6b10fe185cdff76e5
JeongJin984 <57170922+JeongJin984@users.noreply.github.com>
2020-08-11 01:48:10 +0900

    haha

--- front/server.js
+++ front/server.js
@@ -36,7 +36,7 @@ app.prepare()
 			server.use(helmet())
 			server.use(helmet.contentSecurityPolicy({
 				directives: {
-					defaultSrc: ["'self'", "webworks.kr"]
+					defaultSrc: ["'unsafe-inline'"]
 				}
 			}))
 			server.use(cookieParser(process.env.COOKIE_SECRET))
